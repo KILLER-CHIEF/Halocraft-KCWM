841d0bb702a5249e2ccb562a14f3ddf475085a5a
KILLER CHIEF <KILLER_CHIEF@hotmail.com>
2015-09-01 21:51:39 +1000

    Made Grunt flee from player

--- java/net/killerchief/halocraft/entities/mobs/EntityGruntFlee.java
+++ java/net/killerchief/halocraft/entities/mobs/EntityGruntFlee.java
@@ -1,6 +1,7 @@
 package net.killerchief.halocraft.entities.mobs;
 
 import net.killerchief.kcweaponmod.ItemWeapon;
+import net.minecraft.entity.ai.EntityAIAvoidEntity;
 import net.minecraft.entity.ai.EntityAIFollowParent;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAIMate;
@@ -17,28 +18,31 @@ import net.minecraft.world.World;
 public class EntityGruntFlee extends EntityGruntBase {
 
 	private double movementSpeed = 0.3D;
+	private double fleeSpeed = 0.4D;
 	private double runSpeed = 0.5D;
 	
 	public EntityGruntFlee(World par1World) {
 		super(par1World);
 		this.setSize(0.6F, 1.9F);
 		this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(1, new EntityAIPanic(this, this.runSpeed));
-        this.tasks.addTask(2, new EntityAIWander(this, this.movementSpeed));
+		this.tasks.addTask(1, new EntityAIAvoidEntity(this, EntityPlayer.class, 16.0F, this.fleeSpeed, this.runSpeed));
+        this.tasks.addTask(2, new EntityAIPanic(this, this.runSpeed));
+        this.tasks.addTask(3, new EntityAIWander(this, this.movementSpeed));
         for (int i = 0; i < mobWatchList.length; i++)
-        	this.tasks.addTask(3, new EntityAIWatchClosest(this, mobWatchList[i], 6.0F));
-        this.tasks.addTask(4, new EntityAILookIdle(this));
+        	this.tasks.addTask(4, new EntityAIWatchClosest(this, mobWatchList[i], 6.0F));
+        this.tasks.addTask(5, new EntityAILookIdle(this));
 	}
 	
 	public EntityGruntFlee(World par1World, ItemStack helditem) {
 		super(par1World);
 		this.setSize(0.6F, 1.9F);
 		this.tasks.addTask(0, new EntityAISwimming(this));
-		this.tasks.addTask(1, new EntityAIPanic(this, this.runSpeed));
-        this.tasks.addTask(2, new EntityAIWander(this, this.movementSpeed));
+		this.tasks.addTask(1, new EntityAIAvoidEntity(this, EntityPlayer.class, 16.0F, this.fleeSpeed, this.runSpeed));
+        this.tasks.addTask(2, new EntityAIPanic(this, this.runSpeed));
+        this.tasks.addTask(3, new EntityAIWander(this, this.movementSpeed));
         for (int i = 0; i < mobWatchList.length; i++)
-        	this.tasks.addTask(3, new EntityAIWatchClosest(this, mobWatchList[i], 6.0F));
-        this.tasks.addTask(4, new EntityAILookIdle(this));
+        	this.tasks.addTask(4, new EntityAIWatchClosest(this, mobWatchList[i], 6.0F));
+        this.tasks.addTask(5, new EntityAILookIdle(this));
         
         if (!this.worldObj.isRemote)
 			this.setCurrentItemOrArmor(0, helditem);
